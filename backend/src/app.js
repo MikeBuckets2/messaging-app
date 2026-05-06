a7d86015d50eb761d98f4d26a7161b541e431104
MikeBuckets2 <michaelmorris121@yahoo.com>
2026-05-06 17:25:30 +0000

    feat: express app setup

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const errorHandler = require('./middleware/errorHandler');
@@ -24,4 +25,8 @@ app.use((_req, res) => res.status(404).json({ message: 'Route not found.' }));
 
 app.use(errorHandler);
 
-module.exports = app;
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`)
+});
