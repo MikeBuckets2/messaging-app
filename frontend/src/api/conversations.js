f8843393bfee6430b70e214d31c4ec1d793158bc
MikeBuckets2 <michaelmorris121@yahoo.com>
2026-05-08 13:27:46 +0000

    feat: add delete conversation api

--- frontend/src/api/conversations.js
+++ frontend/src/api/conversations.js
@@ -3,6 +3,7 @@ import { get, post, del } from './client';
 export const getMyConversations = () => get('/conversations');
 export const getConversationById = (id) => get(`/conversations/${id}`);
 export const createConversation = (data) => post('/conversations', data);
+export const deleteConversation = (id) => del(`/conversations/${id}`);
 export const addGroupMember = (convId, userId) =>
   post(`/conversations/${convId}/members`, { userId });
 export const removeGroupMember = (convId, userId) =>
